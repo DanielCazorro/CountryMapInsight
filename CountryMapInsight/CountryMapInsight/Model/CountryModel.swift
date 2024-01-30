0779566170c5062f8583ae1578938f621141e0e3
Daniel Cazorro <leinadhunter10@hotmail.com>
2024-01-30 12:55:28 +0100

    Add CountryModel

--- CountryMapInsight/CountryMapInsight/Model/CountryModel.swift
+++ CountryMapInsight/CountryMapInsight/Model/CountryModel.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+import MapKit
+
+// Estructura que representa un país
+struct CountryModel {
+    
+    let name: String  // Nombre del país
+    let capital: String  // Nombre de la capital del país
+    let countryCoordinates: CLLocationCoordinate2D  // Coordenadas del país
+    let capitalCoordinates: CLLocationCoordinate2D  // Coordenadas de la capital del país
+    var likes: Int = 0  // Contador de likes, inicializado en 0
+    
+}
